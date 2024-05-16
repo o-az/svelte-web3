2627379346eecf93ede0bf1de7d5c98a4a1c482f
omar <omaraziz.dev@proton.me>
2024-05-15 22:07:13 -0700

    feat: query storage persister

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,45 @@
 <script lang="ts">
   import '$/app.css'
   import '$/polyfill.ts'
+  import {
+    QueryClient,
+    notifyManager,
+    MutationCache
+  } from '@tanstack/svelte-query'
   import { browser } from '$app/environment'
   import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools'
-  import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query'
+  import { PersistQueryClientProvider } from '@tanstack/svelte-query-persist-client'
+  import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
 
-  const queryClient = new QueryClient({
+  if (browser) notifyManager.setScheduler(window.requestAnimationFrame)
+
+  const queryClient: QueryClient = new QueryClient({
     defaultOptions: {
-      queries: { enabled: browser }
-    }
+      queries: {
+        enabled: browser,
+        gcTime: 1_000 * 60 * 60 * 24, // 24 hours
+        refetchOnReconnect: () => !queryClient.isMutating()
+      }
+    },
+    mutationCache: new MutationCache({
+      onSettled: () => {
+        if (queryClient.isMutating() === 1) {
+          return queryClient.invalidateQueries()
+        }
+      }
+    })
+  })
+
+  const localStoragePersister = createSyncStoragePersister({
+    key: 'SVELTE_QUERY',
+    storage: browser ? window.localStorage : undefined // or window.sessionStorage
   })
 </script>
 
-<QueryClientProvider client={queryClient}>
+<PersistQueryClientProvider
+  client={queryClient}
+  persistOptions={{ persister: localStoragePersister }}
+>
   <slot />
   <SvelteQueryDevtools initialIsOpen={false} />
-</QueryClientProvider>
+</PersistQueryClientProvider>
