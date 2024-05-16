56a769a9c69f23e48ceecdcdee138e53eda11077
omar <omaraziz.dev@proton.me>
2024-05-15 21:26:20 -0700

    chore: update adapter

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,9 @@
 // See https://kit.svelte.dev/docs/types#app
 declare global {
   namespace App {}
+  interface Platform {
+    env: {}
+  }
   interface Window {
     EventEmitter: typeof EventEmitter
   }
